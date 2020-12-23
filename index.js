5c7425804c1de7ba250b8ed27982011b328caac8
heldinho <helderferreirapassos@gmail.com>
2020-12-23 02:52:21 -0200

    fix
    Signed-off-by: heldinho <helderferreirapassos@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 require('dotenv/config')
 const axios = require('axios')
 
-const msg = [
-  'Imagem número 1 acesse o link: https://ibb.co/cL7VFX0',
-  'Imagem número 2 acesse o link: https://ibb.co/FH275hG',
-]
+
 
 async function sms(msg) {
   const sms = await axios({
